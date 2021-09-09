fd9f541e8d688b13f66174ffa9eec40438c63532
Andrew Wang <aywang71@gmail.com>
2021-09-09 16:07:37 -0400

    lab 3

---
+++ Lesson 3/Lab3A_Wang_Andrew.py
@@ -0,0 +1,9 @@
+n = int(input("Input a number:"))
+
+if n%2 == 0:
+    print('even number')
+else:
+    print('odd number')
+
+if n<0:
+    print('negative number')
---
+++ Lesson 3/Lab3B_Wang_Andrew.py
@@ -0,0 +1,8 @@
+count = 0
+number = 0
+
+while count < 20:
+    if number%2 == 0:
+        print(number)
+        count += 1
+    number += 1
